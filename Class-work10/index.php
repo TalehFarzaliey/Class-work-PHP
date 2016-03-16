6a2a247a27dbb8d175445311236e08678607d944
Karim Karimov <kerimovscreations@gmail.com>
2016-03-17 01:53:33 +0400

    Removed bootstrap file, added online link.

--- Class-work10/index.php
+++ Class-work10/index.php
@@ -4,7 +4,7 @@ include 'model.php';
 <html>
 <head>
     <title>Form</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
